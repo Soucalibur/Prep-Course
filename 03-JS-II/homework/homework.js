2a5b672b84be9ef0353a8a88a4506d1981fa87c7
Matthew Gallar <matthewgallar2112@outlook.com>
2022-07-29 22:55:12 -0300

    JSII Done

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -233,7 +233,8 @@ function tablaDelSeis(){
 function tieneTresDigitos(numero){
   //Leer un número entero y retornar true si tiene 3 dígitos. Caso contrario, retorna false.
   //Escribe tu código aquí
-  if (numero > 99 && numero < 1000){
+  var string = numero.toString();
+  if (string.length ===3){
     return true;
   }
   else{
